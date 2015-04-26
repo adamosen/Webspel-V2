7f639d9e2f72c7440ba47f5c935e4afc59508e66
adamosen <adamosen@hotmail.com>
2015-04-26 18:23:53 +0200

    ok

--- Webbspel/Scripts/src/preload.js
+++ Webbspel/Scripts/src/preload.js
@@ -1,29 +1,29 @@
-//var preload = function(game){}
+var preload = function(game){}
 
-//preload.prototype = {
-//	preload: function(){ 
-//          var loadingBar = this.add.sprite(400,240,"loading");
-//          loadingBar.anchor.setTo(0.5,0.5);
-//          this.load.setPreloadSprite(loadingBar);
+preload.prototype = {
+	preload: function(){ 
+          var loadingBar = this.add.sprite(400,240,"loading");
+          loadingBar.anchor.setTo(0.5,0.5);
+          this.load.setPreloadSprite(loadingBar);
 
-//		this.game.load.spritesheet("numbers","/Content/assets/numbers.png",100,100);
-//		this.game.load.image("gametitle", "/Content/assets/gametitle.png");
-//		this.game.load.image("play", "/Content/assets/play.png");
-//		this.game.load.image("higher", "/Content/assets/higher.png");
-//		this.game.load.image("lower", "/Content/assets/lower.png");
-//		this.game.load.image("gameover", "/Content/assets/gameover.png");
+		this.game.load.spritesheet("numbers","/Content/assets/numbers.png",100,100);
+		this.game.load.image("gametitle", "/Content/assets/gametitle.png");
+		this.game.load.image("play", "/Content/assets/play.png");
+		this.game.load.image("higher", "/Content/assets/higher.png");
+		this.game.load.image("lower", "/Content/assets/lower.png");
+		this.game.load.image("gameover", "/Content/assets/gameover.png");
 
-//		this.game.load.tilemap('map', '/Content/gameassets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.tilemap('map', '/Content/gameassets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON);
 
-//		this.game.load.image('burger', '/Content/gameassets/burger.png');
-//		this.game.load.image('tiles2', '/Content/gameassets/tilestest1.png');
-//		this.game.load.image('tree', '/Content/gameassets/tree.png');
+		this.game.load.image('burger', '/Content/gameassets/burger.png');
+		this.game.load.image('tiles2', '/Content/gameassets/tilestest1.png');
+		this.game.load.image('tree', '/Content/gameassets/tree.png');
 
-//		this.game.load.spritesheet('tiles1', '/Content/gameassets/tilestest1.png',32,32);
-//		this.game.load.spritesheet('items', '/Content/gameassets/items.png', 32, 32);
-//		this.game.load.spritesheet('bear', '/Content/gameassets/charsprites.png', 32, 64);
-//	},
-//  	create: function(){
-//		this.game.state.start("GameTitle");
-//	}
-//}
+		this.game.load.spritesheet('tiles1', '/Content/gameassets/tilestest1.png',32,32);
+		this.game.load.spritesheet('items', '/Content/gameassets/items.png', 32, 32);
+		this.game.load.spritesheet('bear', '/Content/gameassets/charsprites.png', 32, 64);
+	},
+  	create: function(){
+		this.game.state.start("GameTitle");
+	}
+}
