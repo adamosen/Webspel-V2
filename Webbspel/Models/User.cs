cc61442c6c357549814627552f4602ac3b6c8cf0
CarolusRex <ludwig.fingal@hotmail.com>
2015-04-27 19:13:07 +0200

    Fortsättning på login
    Lade till en databas där man kan lagra användare och har börjat
    programmera en inloggningsfunktion

---
+++ Webbspel/Models/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Webbspel.Helpers;
+
+namespace Webbspel.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool Remember { get; set; }
+
+        public bool IsValid(string _username, string _password)
+        {
+            using (var cn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename" +
+                @"=|DataDirectory|\UserDatabase.mdf';Integrated Security=True"))
+            {
+                string _sql = @"Select [Username] FROM [dbo].[System_Users] " +
+                    @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = Helpers.SHA1.Encode(_password);
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+
+            }
+        }
+    }
+}
