7051b5d652d040058f8b30c6f8b9aca45fd50e73
Angelica-92 <Angelica_92-@hotmail.com>
2015-05-17 19:44:39 +0200

    Lagt  till Highscore controller

---
+++ Webbspel/Controllers/HighscoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbspel.Controllers
+{
+    public class HighscoreController : Controller
+    {
+        
+        // GET: /Highscore/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
