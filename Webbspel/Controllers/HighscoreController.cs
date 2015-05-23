5501e5981e78fef707dc19121a0fdea8f2bd21ec
CarolusRex <ludwig.fingal@hotmail.com>
2015-05-23 18:50:21 +0200

    Upprensning
    Rensade upp lite i alla filer, tog bort en del som inte användes och
    lade till lite kommentarer

--- Webbspel/Controllers/HighscoreController.cs
+++ Webbspel/Controllers/HighscoreController.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
 
-namespace Webbspel.Controllers
-{
-    public class HighscoreController : Controller
-    {
+//namespace Webbspel.Controllers
+//{
+////    public class HighscoreController : Controller
+////    { 
         
-        // POST: /Highscore/
-        public ActionResult Index();
+//////        // POST: /Highscore/
+//////        public ActionResult Index();
    
-        static void Main(){
+//////        static void Main(){
    
-        }
-        int counter = 0;
-        string line;
+//////        }
+//////        int counter = 0;
+//////        string line;
 
-       // Skriver ut rad för rad från textfilen
-       System.IO.StreamReader file = 
-       new System.IO.StreamReader(@"c:\hscore.txt");
-           while((line = file.ReadLine()) != null)
-      {
-        System.Console.WriteLine (line);
-        counter++;
-        }
+//////       // Skriver ut rad för rad från textfilen
+//////       System.IO.StreamReader file = 
+//////       new System.IO.StreamReader(@"c:\hscore.txt");
+//////           while((line = file.ReadLine()) != null)
+//////      {
+//////        System.Console.WriteLine (line);
+//////        counter++;
+//////        }
 
-        file.Close();
-        System.Console.WriteLine("There were {0} lines.", counter);
-        // Suspend the screen.
-        System.Console.ReadLine();
-        }
+//////        file.Close();
+//////        System.Console.WriteLine("There were {0} lines.", counter);
+//////        // Suspend the screen.
+//////        System.Console.ReadLine();
+//////        }
 
-        static void Main(string [] args)
-{
-            int x = 0;
-            while(x<100)
-            {
-                Console.WriteLine("Highscore:", x)
-            }
-            Console.Read();
-}
+//////        static void Main(string [] args)
+//////{
+//////            int x = 0;
+//////            while(x<100)
+//////            {
+//////                Console.WriteLine("Highscore:", x)
+//////            }
+//////            Console.Read();
+//////}
--- Webbspel/Models/AccountViewModels.cs
+++ Webbspel/Models/AccountViewModels.cs
@@ -2,12 +2,6 @@
 
 namespace Webbspel.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
-    }
 
     public class ManageUserViewModel
     {
@@ -24,7 +18,7 @@ namespace Webbspel.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Compare("NewPassword", ErrorMessage = "The passwords do not match")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -50,14 +44,14 @@ namespace Webbspel.Models
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "The passwords do not match")]
         public string ConfirmPassword { get; set; }
     }
 }
